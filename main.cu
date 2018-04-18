bf13e2dbfea7d6868b3b55385c5beb8b01fe96ab
Chris Jansson <cgj6hb@mail.missouri.edu>
2018-04-18 15:56:22 -0500

    handles 3x3 filter, no edge cases
    lots of hard coding

--- main.cu
+++ main.cu
@@ -30,8 +30,36 @@ int main(int argc, char** argv) {
 
     std::cout << "Successfully loaded PGM image file" << std::endl;
 
-    for (int i = 0; i < 262144; i++) {
-        //pixels[i] = 0;
+        // First, create a 3x3 filter
+	for (int i = 0; i < 512; i++) {
+        for (int j = 0; j < 512; j++) {
+            if (i > 0 && j > 0 && i < 511 & j < 511) {
+                std::vector<unsigned char> v;
+
+                // Index of current pixel
+                int k = i * 512 + j;
+
+                // Top row of neighborhood
+                v.push_back(pixels[k - 513]);
+                v.push_back(pixels[k - 512]);
+                v.push_back(pixels[k - 511]);
+
+                // Middle row of neighborhood
+                v.push_back(pixels[k - 1]);
+                v.push_back(pixels[k]);
+                v.push_back(pixels[k + 1]);
+
+                // Bottom row of neighborhood
+                v.push_back(pixels[k + 511]);
+                v.push_back(pixels[k + 512]);
+                v.push_back(pixels[k + 513]);
+
+                std::sort(v.begin(), v.end());
+
+                int size = v.size();
+                pixels[k] = v[size / 2];
+            }
+        }
     }
 
     if (sdkSavePGM<unsigned char>(argv[3], pixels, w, h) != true)
