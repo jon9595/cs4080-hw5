00097bdbbe89ee00d5f45be0adf95b38af7ae296
Chris Jansson <cgj6hb@mail.missouri.edu>
2018-04-17 23:02:20 -0500

    Load image, alter it and save it back out

---
+++ main.cu
@@ -0,0 +1,26 @@
+int main() {
+	unsigned int w, h;
+	unsigned char* pixels = NULL;
+
+	if (sdkLoadPGM<unsigned char>("lena.pgm", &pixels, &w, &h) != true)
+	{
+	  std::cout << "Unable to load PGM image file" << std::endl;
+	  return 1;
+	}
+
+	std::cout << "Successfully loaded PGM image file" << std::endl;
+
+	for (int i = 0; i < 262144; i++) {
+	  //pixels[i] = 0;
+	}
+
+	if (sdkSavePGM<unsigned char>("lena2.pgm", pixels, w, h) != true)
+	{
+	  std::cout << "Unable to save PGM image file" << std::endl;
+	  return 1;
+	}
+
+	std::cout << "Successfully saved PGM image file" << std::endl;
+	
+	return 0;
+}
