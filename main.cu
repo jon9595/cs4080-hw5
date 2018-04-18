de05b1318fff365a05a6579c9f16ab5a895387a8
Jon Lantsberger <jtl378@mail.missouri.edu>
2018-04-18 20:26:36 +0000

    fixed cmake nonsense and took in args into vars

--- main.cu
+++ main.cu
@@ -1,31 +1,46 @@
 #include <stdio.h>
+#include <string>
 
 #include "cuda_runtime.h"
 #include "lib/helper_image.h"
 
-int main() {
-	unsigned int w, h;
-	unsigned char* pixels = NULL;
+unsigned char* goldenstandard(unsigned char* pixels) {
+    unsigned char* output = NULL;
 
-	if (sdkLoadPGM<unsigned char>("../lena.pgm", &pixels, &w, &h) != true)
-	{
-	  std::cout << "Unable to load PGM image file" << std::endl;
-	  return 1;
-	}
+    return output;
+}
 
-	std::cout << "Successfully loaded PGM image file" << std::endl;
+int main(int argc, char** argv) {
 
-	for (int i = 0; i < 262144; i++) {
-	  //pixels[i] = 0;
-	}
+    if(argc != 4) {
+        std::cout << "Usage ./homework_5 <filter_size> <input_file> <output_file>" << std::endl;
+        exit(1);
+    }
 
-	if (sdkSavePGM<unsigned char>("../lena2.pgm", pixels, w, h) != true)
-	{
-	  std::cout << "Unable to save PGM image file" << std::endl;
-	  return 1;
-	}
+    int filter_size = std::stoi(argv[1]);
 
-	std::cout << "Successfully saved PGM image file" << std::endl;
-	
-	return 0;
+    unsigned int w, h;
+    unsigned char* pixels = NULL;
+
+    if (sdkLoadPGM<unsigned char>(argv[2], &pixels, &w, &h) != true)
+    {
+        std::cout << "Unable to load PGM image file" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Successfully loaded PGM image file" << std::endl;
+
+    for (int i = 0; i < 262144; i++) {
+        //pixels[i] = 0;
+    }
+
+    if (sdkSavePGM<unsigned char>(argv[3], pixels, w, h) != true)
+    {
+        std::cout << "Unable to save PGM image file" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Successfully saved PGM image file" << std::endl;
+
+    return 0;
 }
