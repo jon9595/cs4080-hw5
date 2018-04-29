97c8b14f888b5321817befe63e7306057cf50ec1
Chris Jansson <cgj6hb@mail.missouri.edu>
2018-04-28 23:19:48 -0500

    Fixed name

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef __KERNEL_H__
-#define __KERNEL_H__
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
 void processImageWithCPU(unsigned char*, unsigned int, unsigned int, unsigned int);
 __global__ void processImageWithGPU(unsigned char*, unsigned int*, unsigned int*, unsigned int*);
